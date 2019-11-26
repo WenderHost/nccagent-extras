40302d1dd9a26c262cb37ec2882d9de2f8a07c2d
Michael Wender <michael@michaelwender.com>
2019-11-26 13:32:25 -0500

    Creating new user

--- lib/fns/salesforce.php
+++ lib/fns/salesforce.php
@@ -14,7 +14,16 @@ function process_new_record( $record, $handler ){
   $raw_fields = $record->get( 'fields' );
   $fields = [];
   foreach( $raw_fields as $id => $field ){
-    $fields[$id] = $field['value'];
+    switch( $id ){
+      case 'password':
+      case 'postId':
+        $$id = $field['value'];
+        break;
+
+      default:
+        $fields[$id] = $field['value'];
+    }
+
   }
 
   // Add HS Context variables
@@ -22,10 +31,9 @@ function process_new_record( $record, $handler ){
   if( isset( $_COOKIE['hubspotutk'] ) )
     $hs_context['hutk'] = $_COOKIE['hubspotutk'];
   $hs_context['ipAddress'] = $_SERVER['REMOTE_ADDR'];
-  if( isset( $fields['postId'] ) && is_numeric( $fields['postId'] ) ){
-    $hs_context['pageUrl'] = get_permalink( $fields['postId'] );
-    $hs_context['pageName'] = get_the_title( $fields['postId'] );
-    unset( $fields['postId'] );
+  if( isset( $postId ) && is_numeric( $postId ) ){
+    $hs_context['pageUrl'] = get_permalink( $postId );
+    $hs_context['pageName'] = get_the_title( $postId );
   }
   $fields['hs_context'] = json_encode( $hs_context );
 
@@ -35,7 +43,19 @@ function process_new_record( $record, $handler ){
 
       $args = ['body' => $fields];
       $response = wp_remote_post('https://forms.hubspot.com/uploads/form/v2/' . HS_PORTAL_ID . '/748288ad-0f33-4164-a46b-e394ef7ca982', $args );
-      ncc_error_log('👉 $args = ' . print_r( $args, true ) );
+      if( ! email_exists( $fields['email'] ) ){
+        $user_id = wp_insert_user([
+          'user_pass' => $password,
+          'user_login' => $fields['email'],
+          'user_email' => $fields['email'],
+          'display_name' => $fields['firstname'],
+          'first_name' => $fields['firstname'],
+          'last_name' => $fields['lastname'],
+        ]);
+        add_user_meta( $user_id, 'npn', $fields['npn'], true );
+      } else {
+        ncc_error_log('🔔 A user with the email `' . $fields['email'] . '` already exists!' );
+      }
       break;
 
     default:
