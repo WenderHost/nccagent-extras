df8a7d945ea01a002160dafa265ba8d85320a6fe
Michael Wender <michael@michaelwender.com>
2019-12-04 13:03:27 -0500

    Fixing empty option for plans

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -5,7 +5,7 @@ namespace NCCAgent\enqueues;
 function enqueue_scripts(){
   wp_register_script( 'datatables', '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js', ['jquery'], '1.10.19', true );
   wp_register_script( 'choices', plugin_dir_url( __FILE__ ) . '../js/choices.js', ['jquery'] );
-  wp_register_script( 'datatables-init', plugin_dir_url( __FILE__ ) . '../js/datatables-init.js', ['datatables','choices'], filemtime( plugin_dir_path( __FILE__ ). '../js/datatables-init.js' ), true );
+  wp_register_script( 'datatables-init', plugin_dir_url( __FILE__ ) . '../js/plan-finder.js', ['datatables','choices'], filemtime( plugin_dir_path( __FILE__ ). '../js/plan-finder.js' ), true );
   wp_register_style( 'datatables', '//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css', null, '1.10.19' );
 
   wp_enqueue_script( 'dropdown-side-menu', plugin_dir_url( __FILE__ ) . '../js/menu.js', ['jquery'], filemtime( plugin_dir_path( __FILE__ ) . '../js/menu.js' ), true );
