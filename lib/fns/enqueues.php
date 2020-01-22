684a4379faf7c7347aea8e18e4197137d560c484
Michael Wender <michael@michaelwender.com>
2020-01-22 15:15:37 -0500

    Documenting enqueues

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -6,17 +6,21 @@ namespace NCCAgent\enqueues;
  * Enqueues our scripts and styles.
  */
 function enqueue_scripts(){
-  // Datatables provides the app framework which powers the Plan Finder
+  // DATATABLES: Datatables provides the app framework which powers the Plan Finder
+  // Just datatables: //cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js
+  // Datatables w/ Responsive: //cdn.datatables.net/v/dt/dt-1.10.20/r-2.2.3/datatables.min.js
   wp_register_script( 'datatables', '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js', ['jquery'], '1.10.19', true );
 
-  // We use Select2 for enhanced selects on the Plan Finder
+  // SELECT2: We use Select2 for enhanced selects on the Plan Finder
   wp_register_script( 'select2', 'https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js', ['jquery'], '4.0.12' );
 
-  // My custom JS which initializes the Plan Finder
+  // PLAN FINDER: My custom JS which initializes the Plan Finder
   wp_register_script( 'plan-finder', plugin_dir_url( __FILE__ ) . '../js/plan-finder.js', ['datatables','select2'], filemtime( plugin_dir_path( __FILE__ ). '../js/plan-finder.js' ), true );
 
+  // DROPDOWN SIDEMENU: Used to display Products/Carriers sidenav
   wp_enqueue_script( 'dropdown-side-menu', plugin_dir_url( __FILE__ ) . '../js/menu.js', ['jquery'], filemtime( plugin_dir_path( __FILE__ ) . '../js/menu.js' ), true );
 
+  // DIRECTORY LISTER: Displays links to files from the NCC VPN.
   wp_register_script( 'dirlister', plugin_dir_url( __FILE__ ) . '../js/dirlister.js', ['jquery'], filemtime( plugin_dir_path( __FILE__ ) . '../js/dirlister.js' ), true );
 
   // We currently are locally hosting our Google Fonts, uncomment the following if we want to offload them to the Google CDN:
