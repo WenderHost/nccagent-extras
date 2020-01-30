fa75617bf6d90c5bc6cdd0ea5914d87f6c28d305
Michael Wender <michael@michaelwender.com>
2020-01-30 10:35:26 -0500

    Documenting 1.1.0 with `wp ncc import/export`

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           1.0.3
+ * Version:           1.1.0
  *
  * @package           Nccagent_Extras
  */
