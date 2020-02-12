02242a1c956cfed3a9c4059a45f68e26c30d3ccf
Michael Wender <michael@michaelwender.com>
2020-02-12 06:21:52 -0500

    Moving user marketer association to user meta

--- lib/fns/user-profiles.php
+++ lib/fns/user-profiles.php
@@ -19,6 +19,39 @@ function extra_user_profile_fields( $user ) {
             <span class="description"><?php _e("Please enter your NPN."); ?></span>
         </td>
     </tr>
+    <?php
+    $current_marketer = get_the_author_meta( 'marketer', $user->ID );
+    $marketers = get_posts([
+      'post_type'   => 'team_member',
+      'orderby'     => 'title',
+      'order'       => 'ASC',
+      'numberposts' => -1,
+      'tax_query'   => [
+        [
+          'taxonomy'  => 'staff_type',
+          'field'     => 'slug',
+          'terms'     => 'marketing',
+        ]
+      ],
+    ]);
+    $options[] = '<option value="">Select agents\'s Marketer...</option>';
+    if( $marketers ){
+      foreach( $marketers as $marketer ){
+        //$selected = ( $marketer->ID == $current_marketer )? ' selected="selected"' : '';
+        $options[] = '<option value="' . $marketer->ID . '"' . selected( $marketer->ID, $current_marketer, false ) . '>' . $marketer->post_title . '</option>';
+      }
+    } else {
+      $options[] = '<option value="" selected="selected">No Marketers found.</option>';
+    }
+    $marketer_select = '<select name="marketer" id="marketer">' . implode( '', $options ) . '</select>';
+    ?>
+    <tr>
+        <th><label for="marketer"><?php _e("Marketer"); ?></label></th>
+        <td>
+            <?php echo $marketer_select; ?>
+            <br /><span class="description"><?php _e("Select this agent's marketer."); ?></span>
+        </td>
+    </tr>
     </table>
 <?php
 }
@@ -37,39 +70,40 @@ function save_extra_user_profile_fields( $user_id ) {
       return false;
   }
   update_user_meta( $user_id, 'npn', $_POST['npn'] );
+  update_user_meta( $user_id, 'marketer', $_POST['marketer'] );
 }
 add_action( 'personal_options_update', __NAMESPACE__ . '\\save_extra_user_profile_fields' );
 add_action( 'edit_user_profile_update', __NAMESPACE__ . '\\save_extra_user_profile_fields' );
 
+/**
+ * Displays a user's assigned Team Member (i.e. Marketer)
+ *
+ * @param      <type>  $atts   The atts
+ *
+ * @return     string  Marketer HTML.
+ */
 function my_marketer( $atts ){
   $args = shortcode_atts( [
-    'foo' => 'bar',
+    'id' => null,
   ], $atts );
 
   $user = wp_get_current_user();
   if( ! $user )
-    return '<p><strong>No User Found!</strong>You don\'t appear to be logged in.</p>';
+    return '<p><strong>No User Found!</strong> You don\'t appear to be logged in.</p>';
 
-  $marketers = get_posts([
-    'post_type'     => 'team_member',
-    'meta_query'    => [
-      [
-        'key'     => 'agents',
-        'value'   => serialize( $user->ID ),
-        'compare' => 'LIKE'
-      ]
-    ]
-  ]);
+  $marketer_id = get_user_meta( $user->ID, 'marketer', true );
+  if( ! $marketer_id )
+    return '<p><strong>No Team Member Assigned</strong> No Team Member has been assigned to your user profile. Please contact NCC to have our staff assign a Team Member to you.</p>';
+
+  $marketer = get_post( $marketer_id );
+
   $html = '';
-  foreach( $marketers as $marketer ){
-    $photo = get_the_post_thumbnail_url( $marketer->ID, 'medium' );
-    $marketerFields = get_fields( $marketer->ID, false );
-    $template = file_get_contents( plugin_dir_path( __FILE__ ) . '../html/marketer.html' );
-    $search = [ '{photo}', '{name}', '{title}', '{phone}', '{email}' ];
-    $replace = [ $photo, $marketer->post_title, $marketerFields['title'], $marketerFields['phone'], $marketerFields['email'] ];
-    $html = str_replace( $search, $replace, $template );
-    //$html.= '<div class="marketer user-profile"><class="row">' . $photo . '<h5>Your NCC Contact:</h5><h3>' . $marketer->post_title . '<span class="">' . $marketerFields['title'] . '</span></h3><p>' . $marketerFields['phone'] . ' &bull; <a href="' . $marketerFields['email'] . '">' . $marketerFields['email'] . '</a></p></div></div>';
-  }
+  $photo = get_the_post_thumbnail_url( $marketer->ID, 'medium' );
+  $marketerFields = get_fields( $marketer->ID, false );
+  $template = file_get_contents( plugin_dir_path( __FILE__ ) . '../html/marketer.html' );
+  $search = [ '{photo}', '{name}', '{title}', '{phone}', '{email}' ];
+  $replace = [ $photo, $marketer->post_title, $marketerFields['title'], $marketerFields['phone'], $marketerFields['email'] ];
+  $html = str_replace( $search, $replace, $template );
 
   return $html;
 }
--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           1.4.4
+ * Version:           1.4.5
  *
  * @package           Nccagent_Extras
  */
