a3405399bf590c5aeb297b5cd117cb1215093643
Michael Wender <michael@michaelwender.com>
2020-03-11 09:39:33 -0400

    Adding Carrier > Product paths

--- lib/fns/query_vars.php
+++ lib/fns/query_vars.php
@@ -3,7 +3,7 @@
 namespace NCCAgent\query_vars;
 
 /**
- * Add `productcarrier` and `path` as a query variable.
+ * Add `carrierproduct`, `productcarrier`, and `path` as a query variables.
  *
  * @param      array  $vars   The query variables
  *
@@ -12,15 +12,20 @@ namespace NCCAgent\query_vars;
 function add_query_vars( $vars ){
   $vars[] = 'productcarrier';
   $vars[] = 'path';
+  $vars[] = 'carrierproduct';
   return $vars;
 }
 add_filter( 'query_vars', __NAMESPACE__ . '\\add_query_vars' );
 
 /**
- * Add rewrite which contains the carrier after the product slug/name
+ * Add rewrites:
+ *
+ * - /product/${product}/${carrier}
+ * - /carrier/${carrier}/${product}
  */
 function add_rewrites(){
   add_rewrite_tag( '%productcarrier%', '([0-9a-zA-Z_\-]+)' );
   add_rewrite_rule( 'product/([0-9a-zA-Z_\-]+)/([0-9a-zA-Z_\-]+)/?', 'index.php?post_type=product&name=$matches[1]&productcarrier=$matches[2]', 'top' );
+  add_rewrite_rule( 'carrier/([0-9a-zA-Z_\-]+)/([0-9a-zA-Z_\-]+)/?', 'index.php?post_type=carrier&name=$matches[1]&carrierproduct=$matches[2]', 'top' );
 }
 add_action( 'init', __NAMESPACE__ . '\\add_rewrites', 10, 0 );
