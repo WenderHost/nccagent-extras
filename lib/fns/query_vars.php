b3dafa1db7d7d187fbd86e30d660aac45c07b970
Michael Wender <michael@michaelwender.com>
2020-03-12 16:42:14 -0400

    Adding rewrite tag for `carrierproduct`

--- lib/fns/query_vars.php
+++ lib/fns/query_vars.php
@@ -25,6 +25,7 @@ add_filter( 'query_vars', __NAMESPACE__ . '\\add_query_vars' );
  */
 function add_rewrites(){
   add_rewrite_tag( '%productcarrier%', '([0-9a-zA-Z_\-]+)' );
+  add_rewrite_tag( '%carrierproduct%', '([0-9a-zA-Z_\-]+)' );
   add_rewrite_rule( 'product/([0-9a-zA-Z_\-]+)/([0-9a-zA-Z_\-]+)/?', 'index.php?post_type=product&name=$matches[1]&productcarrier=$matches[2]', 'top' );
   add_rewrite_rule( 'carrier/([0-9a-zA-Z_\-]+)/([0-9a-zA-Z_\-]+)/?', 'index.php?post_type=carrier&name=$matches[1]&carrierproduct=$matches[2]', 'top' );
 }
