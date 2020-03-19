c3a80b6bd1bd94b16e190cd6aa078e1d11074b32
Michael Wender <michael@michaelwender.com>
2020-03-19 09:01:37 -0400

    Removing chevrons from Gravity Forms forms

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           1.7.2
+ * Version:           1.7.3
  *
  * @package           Nccagent_Extras
  */
