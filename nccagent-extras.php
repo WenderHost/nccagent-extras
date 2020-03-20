4cb63d64fb5038596065c7def711b04d561f757c
Michael Wender <michael@michaelwender.com>
2020-03-20 14:39:06 -0400

    Updating Plan Finder and Marketer display

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           1.7.8
+ * Version:           1.7.9
  *
  * @package           Nccagent_Extras
  */
