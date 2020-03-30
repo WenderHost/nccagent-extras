6dde553304e01efb4e6e86cd925b346780243c64
Michael Wender <michael@michaelwender.com>
2020-03-30 10:23:45 -0400

    Adding "Dashboard" link to Top Bar

--- lib/fns/wp_nav_menus.php
+++ lib/fns/wp_nav_menus.php
@@ -2,6 +2,37 @@
 
 namespace NCCAgent\wpnavmenus;
 
+/**
+ * Adds a dashboard link to the Top Bar nav
+ *
+ * @param      string  $items  The menu items
+ * @param      object  $args   The arguments
+ *
+ * @return     string  The nav items.
+ */
+function add_dashboard_link( $items, $args ){
+  $item_classes = 'menu-item menu-item-type-custom menu-item-object-custom my-dashboard';
+
+  if( 'top-bar' == $args->menu ){
+    if( is_user_logged_in() ){
+      $items .= '<li class="' . $item_classes . '"><a class="elementor-item" href="' . site_url( 'dashboard' ) . '">My Dashboard</a></li>';
+    } else {
+      $items .= '<li class="' . $item_classes . '"><a class="elementor-item" href="' . site_url( 'login' ) . '">Log In</a></li>';
+    }
+  }
+  return $items;
+}
+add_filter( 'wp_nav_menu_items', __NAMESPACE__ . '\\add_dashboard_link', 10, 2 );
+
+/**
+ * Adds the menu item's `post_name` as the `id` attribute for the nav menu link.
+ *
+ * @param      array   $atts   The attributes
+ * @param      object  $item   The menu item
+ * @param      object  $args   The menu item arguments
+ *
+ * @return     array   Array of attributes for the menu item.
+ */
 function custom_id_attribute ( $atts, $item, $args ) {
   if( isset( $args->menu ) && 'top-bar' == $args->menu )
       $atts['id'] = $item->post_name;
