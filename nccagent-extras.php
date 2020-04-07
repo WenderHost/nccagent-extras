3d355ec817965d9168f4692e6f227de52c114dda
Michael Wender <michael@michaelwender.com>
2020-04-07 13:47:42 -0400

    Updating .details-link and #reset-form links to match heading font

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.0.7
+ * Version:           2.0.8
  *
  * @package           Nccagent_Extras
  */
