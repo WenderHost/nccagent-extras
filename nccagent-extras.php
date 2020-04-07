58ae71687fd9e1dae13bcaaaeb878245f4eb1d6b
Michael Wender <michael@michaelwender.com>
2020-04-07 08:32:19 -0400

    Documenting 2.0.5

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.0.4
+ * Version:           2.0.5
  *
  * @package           Nccagent_Extras
  */
