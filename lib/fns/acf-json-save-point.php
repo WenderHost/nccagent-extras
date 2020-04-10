bbdb89ce6985417b414eca69c0c22df60845b082
Michael Wender <michael@michaelwender.com>
2020-04-10 13:15:13 -0400

    Loading ACF JSON for sync

--- lib/fns/acf-json-save-point.php
+++ lib/fns/acf-json-save-point.php
@@ -1,10 +1,22 @@
 <?php
 
 function ncc_acf_json_save_point( $path ) {
-    // update path
-    $path = plugin_dir_path( __FILE__ ) . '../acf-json';
+  // update path
+  $path = plugin_dir_path( __FILE__ ) . '../acf-json';
 
-    // return
-    return $path;
+  // return
+  return $path;
 }
 add_filter('acf/settings/save_json', 'ncc_acf_json_save_point');
+
+function ncc_acf_json_load_point( $paths ) {
+    // remove original path
+    unset($paths[0]);
+
+    // append path
+    $paths[] = plugin_dir_path( __FILE__ ) . '../acf-json';
+
+    // return
+    return $paths;
+}
+add_filter('acf/settings/load_json', 'ncc_acf_json_load_point');
