d0a837f1950c4ed833523a9a0c2ecd652ac7eac9
Michael Wender <michael@michaelwender.com>
2020-04-13 11:50:05 -0400

    HOTFIX: Marketer avatar was overflowing into Product Finder

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.1.4
+ * Version:           2.1.5
  *
  * @package           Nccagent_Extras
  */
