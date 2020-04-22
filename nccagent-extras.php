769c3b037afdcb9651384d5cf628387ed2bbe482
Michael Wender <michael@michaelwender.com>
2020-04-22 17:48:53 -0400

    Setting ul > li bullets to chevron-circle-right

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.3.6
+ * Version:           2.3.7
  *
  * @package           Nccagent_Extras
  */
