b3cf5d68dfaf303e7917f8bb79a9302d130ba717
Michael Wender <michael@michaelwender.com>
2020-04-24 06:12:23 -0400

    Bumping Carrier Docs min-height to 1200px

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.4.0
+ * Version:           2.4.1
  *
  * @package           Nccagent_Extras
  */
