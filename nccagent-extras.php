ac6b9729b9675b68c9dd122fd5af45e721ad09bc
Michael Wender <michael@michaelwender.com>
2020-05-15 07:53:54 -0400

    Product Finder tweaks

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.6.2
+ * Version:           2.7.0
  *
  * @package           Nccagent_Extras
  */
