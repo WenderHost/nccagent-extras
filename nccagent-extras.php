f2e3694452cba63e3e8cf517e904b40d43bbc81f
Michael Wender <michael@michaelwender.com>
2020-05-19 08:28:06 -0400

    Bug fix: Properly escaping ${selectedState} in Product Finder

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.7.6
+ * Version:           2.7.7
  *
  * @package           Nccagent_Extras
  */
