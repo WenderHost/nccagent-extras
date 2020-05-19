bb438375e0bb74f07c87b8251794a06065c50a2e
Michael Wender <michael@michaelwender.com>
2020-05-19 07:45:33 -0400

    Updating image bullet selector specificity

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.7.3
+ * Version:           2.7.4
  *
  * @package           Nccagent_Extras
  */
