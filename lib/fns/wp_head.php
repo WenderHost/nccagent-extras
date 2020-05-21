19f41a3a7b8aebd3e66c2184eee43e9fb11c940b
Michael Wender <michael@michaelwender.com>
2020-05-21 15:00:57 -0400

    Removing automatic tel: linking on mobile

--- lib/fns/wp_head.php
+++ lib/fns/wp_head.php
@@ -6,3 +6,8 @@ function ie11_compat(){
   echo '<meta http-equiv="X-UA-Compatible" content="IE=edge;" />' . "\n";
 }
 add_action( 'wp_head', __NAMESPACE__ . '\\ie11_compat' );
+
+function remove_mobile_tel_links(){
+  echo '<meta name="format-detection" content="telephone=no">' . "\n";
+}
+add_action( 'wp_head', __NAMESPACE__ . '\\remove_mobile_tel_links' );
--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.8.4
+ * Version:           2.8.5
  *
  * @package           Nccagent_Extras
  */
