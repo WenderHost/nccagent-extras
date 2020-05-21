56a1740e89b8da3c824740fd6d899b6910811fc7
Michael Wender <michael@michaelwender.com>
2020-05-21 14:01:43 -0400

    Updating Product Finder accordions

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           2.8.1
+ * Version:           2.8.2
  *
  * @package           Nccagent_Extras
  */
