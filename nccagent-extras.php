0ddf4be3bb3f961817d4b390eff599f55393f94a
Michael Wender <michael@michaelwender.com>
2021-02-03 11:21:32 -0500

    Adding .BambooHR-ATS-Jobs-List to ul bullet exclusions

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           3.7.6
+ * Version:           3.7.7
  *
  * @package           Nccagent_Extras
  */
