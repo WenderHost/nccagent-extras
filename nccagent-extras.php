d0dccf940489316ccc566ff4ad244592b825c129
Michael Wender <michael@michaelwender.com>
2021-02-03 11:18:29 -0500

    Adding CSS exclusion for job listings bullets

--- nccagent-extras.php
+++ nccagent-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       nccagent-extras
  * Domain Path:       /languages
- * Version:           3.7.5
+ * Version:           3.7.6
  *
  * @package           Nccagent_Extras
  */
