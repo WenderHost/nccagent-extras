ccb32f34cebc3dd8ebbd143a4b7c6c117dae246f
Michael Wender <michael@michaelwender.com>
2022-02-14 18:34:00 -0500

    Adding ncc_get_state_name()

--- lib/fns/utilities.php
+++ lib/fns/utilities.php
@@ -89,6 +89,24 @@ function ncc_get_state_options(){
 }
 
 /**
+ * Given a state abbreviation, returns the full name of the state.
+ *
+ * @param      string      $abbr   The abbr
+ *
+ * @return     string|bool  Returns the state name. FALSE on failure.
+ */
+function ncc_get_state_name( $abbr = null ){
+  if( is_null( $abbr) )
+    return false;
+
+  $state_names = [ 'AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas', 'CA' => 'California', 'CO' => 'Colorado', 'CT' => 'Connecticut', 'DE' => 'Delaware', 'FL' => 'Florida', 'GA' => 'Georgia', 'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas', 'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland', 'MA' => 'Massachusetts', 'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MI' => 'Missouri', 'MT' => 'Montana', 'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey', 'NM' => 'New Mexico', 'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio', 'OK' => 'Oklahoma', 'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota', 'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia', 'WA' => 'Washington', 'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming' ];
+  if( array_key_exists( strtoupper( $abbr ), $state_names ) )
+    return $state_names[$abbr];
+
+  return false;
+}
+
+/**
  * Returns an HTML template from `lib/html/`
  *
  * @param      array  $atts {
