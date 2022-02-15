2d3fe7f98997b725316355f2f08ecf8dc1bd7564
Michael Wender <michael@michaelwender.com>
2022-02-15 16:08:23 -0500

    Adding wordpress_and_activecampaign_registration form validation

--- lib/fns/activecampaign.php
+++ lib/fns/activecampaign.php
@@ -110,3 +110,38 @@ function register_user_and_send_lead_to_activecampaign( $record, $handler ){
   }
 }
 add_action( 'elementor_pro/forms/new_record', __NAMESPACE__ . '\\register_user_and_send_lead_to_activecampaign', 10, 2 );
+
+/**
+ * Validates the User Registration fortm.
+ *
+ * @param      object  $record   The record
+ * @param      object  $handler  The handler
+ *
+ * @return     bool    Returns `false` if global constants are not in the system.
+ */
+function validate_user_registration( $record, $handler ){
+  if( ! defined( 'ACTIVECAMPAIGN_API_KEY' ) ){
+    ncc_error_log('🚨 `ACTIVECAMPAIGN_API_KEY` not defined. Please add your ActiveCampaign API Key to `wp-config.php`.');
+    return false;
+  }
+
+  // Only process the form named `wordpress_and_activecampaign_registration`:
+  $form_name = $record->get_form_settings( 'form_name' );
+  if( 'wordpress_and_activecampaign_registration' != $form_name )
+    return;
+
+  $email_field = $record->get_field([
+    'id' => 'email',
+  ]);
+  $npn_field = $record->get_field([
+    'id' => 'npn',
+  ]);
+
+  if( email_exists( $email_field['email']['value'] ) ){
+    $handler->add_error( $email_field['email']['id'], 'This email address is already in use in our system.' );
+  }
+  if( username_exists( $npn_field['npn']['value'] ) ){
+    $handler->add_error( $npn_field['npn']['id'], 'That NPN is already registered in our system.' );
+  }
+}
+add_action( 'elementor_pro/forms/validation', __NAMESPACE__ . '\\validate_user_registration', 10, 2 );
